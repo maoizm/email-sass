20b360b706b8fe5983d80e67f4eb8feb1e2d855c
Андрій Мельник < >
2016-09-06 17:21:28 +0300

    Independence day

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -107,7 +107,7 @@ function server(done) {
   browser.init({
         server: {
             baseDir: 'dist',
-            index: "20160729_letter_RDPsecurity.html"
+            index: "20160819_letter_IndependenceDay.html"
         }
   });
   done();
